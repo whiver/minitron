cd6c01b2905c65ca8fa0f22a043d1783a1526404
nathan-K- <nathan.kienlen@gmail.com>
2016-11-21 11:01:14 +0100

    dimension variable

--- backend/source/minitron.pl
+++ backend/source/minitron.pl
@@ -1,5 +1,9 @@
 %%% MiniTron %%%
 
-%Pour recuperer un element d une liste vue comme une matrice (!! ici la dimension 3 est en dur)
-operation(X,Y,R) :- R is ((X*3)+Y).
+%Pour recuperer un element d une liste vue comme une matrice
+
+dim(3). %On ne prendra que des matrice carrees
+
+operation(X,Y,R) :- dim(D), R is ((X*D)+Y).
 matrice(X,Y,List, Element) :- operation((X-1),(Y-1),R), nth0( R, List, Element, _).
+
