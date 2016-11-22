58baf0a747d53d4056cd25008e8da32baafa54b6
nathan-k- <nathan.kienlen@gmail.com>
2016-11-22 22:42:12 +0100

    refactor init

--- backend/source/game.pl
+++ backend/source/game.pl
@@ -25,12 +25,4 @@ play :- write('\33\[2J'),
 			sleep(0.5),
 			play.
 
-%Init pourrav pour tester
-init :- assert(dim(10)),
-	dim(D), N is D*D, %Calcul des dimensions
-	X1 is 1, Y1 is 1, X2 is 10, Y2 is 10, %définition des points des 2 têtes
-	matrice(X1, Y1, Board, 1), matrice(X2, Y2, Board, 2), %Placement des têtes dans la matrice
-	initList(Board, N), %initialisation du reste de la matrice avec des 0
-	assert(board(Board, [X1, Y1], [X2, Y2])), %assertion du fait board
-	displayBoard, %Affichage (Remplacer plus tard par le lancement du jeu)
-	play.
+
---
+++ backend/source/utils.pl
@@ -0,0 +1,8 @@
+:- dynamic board/3, dim/1. % permet l'assertion et le retrait de faits board/3 (Board + les 2 têtes)
+
+%Pour recuperer un element d une liste vue comme une matrice
+extraire(X,Y,R) :- dim(D), R is ((X*D)+Y).
+% TODO : ATTENTION : ici X est associe à "X-1", et Y à "Y-1" pour des appels plus "naturels", à discuter
+matrice(X,Y,List, Element) :- extraire((X-1),(Y-1),R), nth0( R, List, Element, _).
+
+
