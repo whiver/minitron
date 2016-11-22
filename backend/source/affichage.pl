b5790863036bd663a0ea521eb792ca98cd5b2ed5
Adrien Coppola <adrien.coppola@gmail.com>
2016-11-22 22:38:35 +0100

    Ajout de qq commentaires et suppression de mon superbe prédicat d'init :(

--- backend/source/affichage.pl
+++ backend/source/affichage.pl
@@ -2,7 +2,8 @@
 %Affiche toute la board
 displayBoard :- board(Board, _, _), displayElem(Board, 0).
 
-%Affiche un élément et lance l’affichage de l'élément suivant
+% Affiche un élément et lance l’affichage de l'élément suivant
+% Affiche 0 si l'élement n'est pas défini
 displayElem([], _).
 displayElem([H|T], I) :- dim(D), I is D-1, ((nonvar(H), writeln(H)); writeln(0)), displayElem(T, 0).
 displayElem([H|T], I) :- ((nonvar(H), write(H)); write(0)), succ(I, Next), displayElem(T, Next).
