d802fdb0e46d8d9f3869087f9b19b933bd784368
NaimaHAMOUMA <naimahamouma@outlook.fr>
2016-11-22 23:08:02 +0100

    Plus de whereInBoard

--- backend/source/iaRandom.pl
+++ backend/source/iaRandom.pl
@@ -21,17 +21,6 @@ move(1,X,Y,NewX,NewY) :- NewX is X, NewY is Y+1.
 move(2,X,Y,NewX,NewY) :- NewX is X+1, NewY is Y.
 move(3,X,Y,NewX,NewY) :- NewX is X, NewY is Y-1.
 
-% se placer sur la board 
-whereInBoard(X,Y,N,[0,1,2,3]) :-  X \== N, Y \== N, X \== 1, Y \== 1 .
-whereInBoard(1,1,_,[1,2]).
-whereInBoard(1,Y,N,[1,2,3]) :- Y \== 1, Y \== N.
-whereInBoard(1,N,N,[2,3]).
-whereInBoard(X,N,N,[0,2,3]) :- X \== 1, X \== N.
-whereInBoard(N,N,N,[0,3]).
-whereInBoard(N,Y,N,[0,1,3]) :-  Y \== 1, Y \== N.
-whereInBoard(N,1,N,[0,1]) .
-whereInBoard(X,1,N,[0,1,2]) :- X \== 1, X \== N.
-
 % IA calculant le prochain déplacement 
-nextMove(X,Y,NewX,NewY,Size) :-  whereInBoard(X,Y,Size,Possibilities),random_member(N,Possibilities), move(N,X,Y,NewX,NewY).
-ia(Board, [M1,M2],[P1,P2]) :- dim(N),nextMove(P1,P2,M1,M2,N).
+nextMove(X,Y,NewX,NewY) :- random_member(N,[0,1,2,3]), move(N,X,Y,NewX,NewY).
+ia(Board, [M1,M2],[P1,P2]) :- dim(N),nextMove(P1,P2,M1,M2).
