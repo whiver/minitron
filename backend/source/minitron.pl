166d7ca4e1f40f4888252202554696dcb3fc8eb9
absouss <absoussdev@gmail.com>
2016-11-22 18:22:22 +0100

    heads au lieu de head1/head2

--- backend/source/minitron.pl
+++ backend/source/minitron.pl
@@ -104,10 +104,11 @@ boardToJSON :-
 	format('"board":[', []),
 	boardElemToJSON(Board), 
 	format(']', []),
-	format(',"head1":', []),
+	format(',"heads":[', []),
 	headToJSON(H1),
-	format(',"head2":', []),
-	headToJSON(H2).
+	format(',', []),
+	headToJSON(H2),
+	format(']', []).
 
 % Convertit un élément du board en JSON et convertit le suivant
 boardElemToJSON([]).
