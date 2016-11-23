a2ad1c096b524de4cd4cd81509f1a8a456fa4d9d
bougnoule <ab_souss@hotmail.com>
2016-11-23 08:57:07 +0100

    Http json fix (#2)
    * initialBoardState
    
    * Refactor de http.pl

--- backend/source/http.pl
+++ backend/source/http.pl
@@ -5,30 +5,37 @@
 
 server(Port) :-
         http_server(http_dispatch,
-                    [ port(Port) ]).
+                    [port(Port)]).
 
-:- http_handler(root(boardState), httpBoardState, []).
+:- http_handler(root(initialBoardState), httpInitialBoardState, []).
 
-% Convertit le board en JSON
-boardToJSON :-
-	board(Board, H1, H2),
-	format('"board":[', []),
-	boardElemToJSON(Board), 
-	format(']', []),
-	format(',"heads":[', []),
+% Formate le board en JSON
+% Ne retourne que la taille d'un côté et les têtes des joueurs
+initialBoardToJSON :-
+	sizeToJSON,
+	format(',', []),
+	headsToJSON.
+
+% Formate la taille d'un côté du board en JSON
+sizeToJSON :-
+	dim(Size),
+	format('"size":~w', [Size]).
+
+% Formate les têtes des joueurs en JSON
+headsToJSON :-
+	format('"heads":[', []),
+	board(_, H1, H2),
 	headToJSON(H1),
 	format(',', []),
 	headToJSON(H2),
 	format(']', []).
 
-% Convertit un élément du board en JSON et convertit le suivant
-boardElemToJSON([]).
-boardElemToJSON([H|T]) :- format('~w,', [H]), boardElemToJSON(T).
-
+% Formate une tête d'un joueur en JSON
 headToJSON([H|[T|_]]) :- format('{"x":~w,"y":~w}', [H, T]).
 
-httpBoardState(_) :-
+% Méthode appelée sur l'URL '/initialBoardState'
+httpInitialBoardState(_) :-
 	format('Content-type: application/json~n~n', []),
 	format('{', []),
-	boardToJSON,
+	initialBoardToJSON,
 	format('}', []).
